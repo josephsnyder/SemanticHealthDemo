48b6332c5db888055d6fb88c6ab12c2d52a7c0ac
Wesley Turner <wes.turner@kitware.com>
2013-08-15 17:09:13 -0400

    Jason fixed the live queries. I am using them.
    We removed the dependency on the data files and are now using live queries.

--- service.py
+++ service.py
@@ -4,4 +4,6 @@ def run():
 	s1 = urllib2.urlopen(req).read()
 	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010%20FILTER(.05=9000001-198)")
 	s2 = urllib2.urlopen(req).read()
-	return "[" + s1 + "," + s2 + "]"
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-198)")
+	s3 = urllib2.urlopen(req).read()
+	return "[" + s1 + "," + s2 + "," + s3 + "]"
