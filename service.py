db0849445ef7cc6b6fd1b34507a1727a1f13a4be
Joseph Snyder <joe.snyder@kitware.com>
2013-08-16 12:27:58 -0400

    ENH: Reorder dropdown Menu and and New Querys
    Reorder the dropdown menu to be in numerical order.
    
    Add the queries and corresponding D3 code for the Patient Allergies
    (120_8) and Prescriptions (52).

--- service.py
+++ service.py
@@ -6,4 +6,8 @@ def run(patientIEN=198):
 	s2 = urllib2.urlopen(req).read()
 	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-"+patientIEN+")")
 	s3 = urllib2.urlopen(req).read()
-	return "[" + s1 + "," + s2 + "," + s3 + "]"
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_8%20FILTER(.01=2-"+patientIEN+")")
+	s4 = urllib2.urlopen(req).read()
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%2052%20FILTER(2=2-"+patientIEN+")")
+	s5 = urllib2.urlopen(req).read()
+	return "[" + s1 + "," + s2 + "," + s3 + "," + s4  + "," + s5 + "]"
