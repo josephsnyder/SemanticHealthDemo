9285df3c4150a719b1e1414cc653c1613f3a873a
Joseph Snyder <joe.snyder@kitware.com>
2013-08-16 10:46:23 -0400

    ENH: Add dropdown menu to select patient IEN
    Add a drop-down menu to the visualization to select a patient IEN from a
    list and then query the database for that patients information.
    
    The patients in the list were chosen at random, but all show valid sets of
    information.

--- service.py
+++ service.py
@@ -1,9 +1,9 @@
 import urllib2
-def run():
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000011%20FILTER(.02=9000001-198)")
+def run(patientIEN=198):
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000011%20FILTER(.02=9000001-"+patientIEN+")")
 	s1 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010%20FILTER(.05=9000001-198)")
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010%20FILTER(.05=9000001-"+patientIEN+")")
 	s2 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-198)")
+	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-"+patientIEN+")")
 	s3 = urllib2.urlopen(req).read()
 	return "[" + s1 + "," + s2 + "," + s3 + "]"
