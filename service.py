818020952215684c85feb323a890730ee37c49f6
Joseph Snyder <joe.snyder@kitware.com>
2013-08-19 14:35:57 -0400

    ENH: Tab removal and add final patient information Queries
    Add queries for the immunizations and the CPT Code files that are
    similarly filtered by a patient IEN.
    
    Add a new option in the dropdown menu to ensure that at least one option
    has information in these two new queries.
    
    Add the files to index.html to handle and process the new returns.
    
    Find and replace all \t in the two files and replace them with two
    spaces.

--- service.py
+++ service.py
@@ -1,13 +1,17 @@
 import urllib2
 def run(patientIEN=198):
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000011%20FILTER(.02=9000001-"+patientIEN+")")
-	s1 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010%20FILTER(.05=9000001-"+patientIEN+")")
-	s2 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-"+patientIEN+")")
-	s3 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_8%20FILTER(.01=2-"+patientIEN+")")
-	s4 = urllib2.urlopen(req).read()
-	req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%2052%20FILTER(2=2-"+patientIEN+")")
-	s5 = urllib2.urlopen(req).read()
-	return "[" + s1 + "," + s2 + "," + s3 + "," + s4  + "," + s5 + "]"
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000011%20FILTER(.02=9000001-"+patientIEN+")")
+  s1 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010%20FILTER(.05=9000001-"+patientIEN+")")
+  s2 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_5%20FILTER(.02=2-"+patientIEN+")")
+  s3 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%20120_8%20FILTER(.01=2-"+patientIEN+")")
+  s4 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%2052%20FILTER(2=2-"+patientIEN+")")
+  s5 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010_11%20FILTER(.02=9000001-"+patientIEN+")")
+  s6 = urllib2.urlopen(req).read()
+  req = urllib2.Request(url="http://www.semantichealthcare.info/fmqlEP?fmql=DESCRIBE%209000010_18%20FILTER(.02=9000001-"+patientIEN+")")
+  s7 = urllib2.urlopen(req).read()
+  return "[" + s1 + "," + s2 + "," + s3 + "," + s4  + "," + s5 + "," + s6 + "," + s7 +"]"
